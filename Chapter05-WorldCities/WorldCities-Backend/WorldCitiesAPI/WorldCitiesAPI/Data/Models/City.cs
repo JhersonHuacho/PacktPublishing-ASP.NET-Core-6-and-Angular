e762660accd089916681b40d83f63c06fef5f1b9
Francisco <jhersonhuacho@gmail.com>
2024-09-18 16:54:41 -0500

    WorldCities-Api: instalar EF Core y agregar la clase City

---
+++ Chapter05-WorldCities/WorldCities-Backend/WorldCitiesAPI/WorldCitiesAPI/Data/Models/City.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorldCitiesAPI.Data.Models
+{
+	public class City
+	{
+		#region Properties
+		/// <summary>
+		/// The unique id and primary key for this City
+		/// </summary>
+		[Key]
+		[Required]
+		public int Id { get; set; }
+		/// <summary>
+		/// City name (in UTF8 format)
+		/// </summary>
+		public string Name { get; set; } = null!;
+		/// <summary>
+		/// City latitude
+		/// </summary>
+		[Column(TypeName = "decimal(7,4)")]
+		public decimal Lat { get; set; }
+		/// <summary>
+		/// City longitude
+		/// </summary>
+		[Column(TypeName = "decimal(7,4)")]
+		public decimal Lon { get; set; }
+		/// <summary>
+		/// Country Id (foreign key)
+		/// </summary>
+		public int CountryId { get; set; }
+		#endregion
+	}
+}
