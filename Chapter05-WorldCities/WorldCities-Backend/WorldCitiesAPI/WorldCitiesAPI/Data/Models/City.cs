63d5c75a31bbb663a3d5fde0ebde89541e59f15f
Francisco <jhersonhuacho@gmail.com>
2024-09-20 10:34:22 -0500

    WorldCities: API: agregar configuración dbcontext y entities.

--- Chapter05-WorldCities/WorldCities-Backend/WorldCitiesAPI/WorldCitiesAPI/Data/Models/City.cs
+++ Chapter05-WorldCities/WorldCities-Backend/WorldCitiesAPI/WorldCitiesAPI/Data/Models/City.cs
@@ -1,8 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace WorldCitiesAPI.Data.Models
 {
+	[Table("Cities")]
+	[Index(nameof(Name))]
+	[Index(nameof(Lat))]
+	[Index(nameof(Lon))]
 	public class City
 	{
 		#region Properties
@@ -29,7 +34,15 @@ namespace WorldCitiesAPI.Data.Models
 		/// <summary>
 		/// Country Id (foreign key)
 		/// </summary>
+		[ForeignKey(nameof(Country))]
 		public int CountryId { get; set; }
+		#endregion
+
+		#region Navigation Properties
+		/// <summary>
+		/// The country related to this city.
+		/// </summary>
+		public Country? Country { get; set; } = null!;
 		#endregion
 	}
 }
